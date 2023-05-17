5b18e0ab479b222398189122e03418c8a41cbad5
Matthew Brett <matthew.brett@gmail.com>
2023-05-17 14:42:04 +0100

    Write templated script, previous version
    Just use config YAML for variables.

--- write_templated.py
+++ write_templated.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python3
-""" Write templated text
+""" Write templated document using config variables
 """
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-
-def args2dict(args):
-    out = {}
-    for m in dir(args):
-        if not m.startswith('_'):
-            val = getattr(args, m)
-            if val is not None:
-                out[m] = val
-    return out
+from mcpmark.mcputils import get_component_config
 
 
 def get_parser():
     parser = ArgumentParser(description=__doc__,  # Usage from docstring
                             formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument('in_fname',
-                        help='Text for templating')
-    parser.add_argument('--hub-url', default='https://ds.lis.2i2c.cloud',
-                        help='Base URL for JupyterHub')
-    parser.add_argument('--hub-sdir', default='projects',
-                        help="Subdirectory for marked files in user's system")
-    parser.add_argument('--marker-email', default='matthew.brett@lis.ac.uk',
-                        help="Contact email for feedback.")
+    parser.add_argument('template_path',
+                        help='Path containing input template')
+    parser.add_argument('out_path',
+                        help='Output document path')
     return parser
 
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    with open(args.in_fname, 'rt') as fobj:
-        out_markdown = fobj.read().format(**args2dict(args))
-    print(out_markdown)
+    args, config = get_component_config(get_parser(),
+                                        multi_component=True,
+                                        component_default='all',
+                                        component_as_option=True)
+    with open(args.template_path, 'rt') as fobj:
+        content = fobj.read()
+    with open(args.out_path, 'wt') as fobj:
+        fobj.write(content.format(**config))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
