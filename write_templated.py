b5ee5a68f0fdc930fcb29b341896bf444d644736
Matthew Brett <matthew.brett@gmail.com>
2023-05-18 12:44:44 +0100

    Stdout by default, explicit YaML file

--- write_templated.py
+++ write_templated.py
@@ -4,28 +4,34 @@
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-from mcpmark.mcputils import get_component_config
+import yaml
+
 
 
 def get_parser():
     parser = ArgumentParser(description=__doc__,  # Usage from docstring
                             formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('config_path', default='projects.yaml',
+                        help='Path to YAML file containing variables')
     parser.add_argument('template_path',
                         help='Path containing input template')
-    parser.add_argument('out_path',
-                        help='Output document path')
+    parser.add_argument('--out_path', default='-',
+                        help='Output document path or - for stdout (default)')
     return parser
 
 
 def main():
-    args, config = get_component_config(get_parser(),
-                                        multi_component=True,
-                                        component_default='all',
-                                        component_as_option=True)
+    args = get_parser().parse_args()
+    with open(args.config_path, 'rt') as fobj:
+        config = yaml.load(fobj, Loader=yaml.SafeLoader)
     with open(args.template_path, 'rt') as fobj:
         content = fobj.read()
-    with open(args.out_path, 'wt') as fobj:
-        fobj.write(content.format(**config))
+    output = content.format(**config)
+    if args.out_path == '-':
+        print(output)
+    else:
+        with open(args.out_path, 'wt') as fobj:
+            fobj.write(output)
 
 
 if __name__ == "__main__":
